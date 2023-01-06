f3971446a612b0d5987e258253fe173416f6be3d
Henry Tregidgo <h.tregidgo@ucl.ac.uk>
2023-01-06 15:09:06 +0000

    Generalise test script

--- scripts/testScripts/testgenerator.py
+++ scripts/testScripts/testgenerator.py
@@ -5,35 +5,46 @@ import torch
 import time
 from joint_diffusion_structural_seg.generators import image_seg_generator, image_seg_generator_rgb, image_seg_generator_rgb_validation
 
+## machine specific directories
+top_level_training_dir = '/media/henry/_localstore/Brain/synthDTI/large_download/training_reduced'
+top_level_model_dir = '/media/henry/_localstore/Brain/synthDTI/models'
+
+## Run specific parameters that will change for ablations
+# Name of the model - link to ablation spreadsheet
+model_name = 'joint_thalamus_test_Jan'
+# Fraction of DTI voxels to randomised. Between 0 and 1. Set to 0 to turn off speckle. 1 in 10k sounds right
+speckle_frac_selected=1e-4
+# Flag whether we'll individually rotate the DTI vectors
+nonlinear_rotation=True
+# Will we deform the images with piecewise linear displacement fields (this includes re-orientation)
+flag_deformation = True
+# Maximimum piecewise linear displacement in mm (excluding rotation + scaling)
+deformation_max = 5.0
+# How to encode the segmentations into a onehot
+# single (pick an example), combined (average), mode (majority vote), grouped (majority vote on group then average)
+seg_selection='grouped'
+# Dice version - "individual" do standard label-wise Dice, "grouped" also add contribution of groups and whole thalamus
+dice_version="grouped"
+
+
+## Paths
 # Path with training data
-#training_dir = '/autofs/space/panamint_005/users/iglesias/data/joint_diffusion_structural_seg/proc_training_data/'
-# training_dir = '/home/henry/Documents/Brain/synthDTI/4henry/data/training_withValidation/train/'
-# validation_dir = '/home/henry/Documents/Brain/synthDTI/4henry/data/training_withValidation/validate/'
-# training_dir = '/home/henry/Documents/Brain/synthDTI/4henry/data/training_reduced/train/'
-# validation_dir = '/home/henry/Documents/Brain/synthDTI/4henry/data/training_reduced/validate/'
-training_dir = '/media/henry/_localstore/Brain/synthDTI/large_download/training_reduced/train/'
+training_dir = os.path.join(top_level_training_dir,'train/')
+# Path with Validation data, set to none if not doing online validation
+#validation_dir = os.path.join(top_level_training_dir,'validate/')
 validation_dir = None
-# validation_dir = None
 # NPY file with list of labels
-# path_label_list = '/home/henry/Documents/Brain/synthDTI/4henry/data/proc_training_data_label_list.npy'
-# path_label_list = '/home/henry/Documents/Brain/synthDTI/4henry/data/proc_training_data_label_list_reduced.npy'
-path_label_list = '/media/henry/_localstore/Brain/synthDTI/large_download/training_reduced/proc_training_data_label_list_reduced.npy'
+path_label_list = os.path.join(top_level_training_dir,'proc_training_data_label_list_reduced.npy')
+# NPY file with segmentation of onehot channels into groups for mixed Dice etc.
+path_group_list = os.path.join(top_level_training_dir,'proc_training_group_seg_reduced.npy')
 # Directory where model files will be written
-# model_dir = '/home/henry/Documents/Brain/synthDTI/4henry/joint_diffusion_structural_seg/models/diffusion_thalamus_test_LabelLossWithWholeThaldebug/'
-model_dir = '/media/henry/_localstore/Brain/synthDTI/models/diffusion_thalamus_new_year_run'
-# NPY file with segmentation of channels into groups
-# path_group_list = '/home/henry/Documents/Brain/synthDTI/4henry/data/proc_training_group_seg.npy'
-path_group_list = '/media/henry/_localstore/Brain/synthDTI/large_download/training_reduced/proc_training_group_seg_reduced.npy'
-# Batch size being volumes, it will probably be always 1...
-batchsize = 1
-# Size to which inputs will be cropped (use None to use whole volume)
-crop_size = 128
+model_dir = os.path.join(top_level_model_dir,model_name)
+
+## Generator specific parameters that remain constant between runs
 # Maximum scaling during augmentation, in [0,1]. 0.15 is a good value
 scaling_bounds = 0.15
 # Maximum rotation during augmentation, in degrees. 15 is a good value
 rotation_bounds = 15
-# Flag whether we'll individually rotate the DTI vectors
-nonlinear_rotation=True
 # Maximum standard deviation of noise to add in augmentation.
 # Since images are normalized, 0.1 is a good value for t1 (a bit lower for FA, since it's a fit already)
 max_noise_std = 0.1
@@ -45,20 +56,17 @@ contrast_std = 0.1
 brightness_std = 0.1
 # Randomize resolution during training?
 randomize_resolution = True
-# Fraction of DTI voxels to randomise
-speckle_frac_selected=1e-4
-# How to encode the segs into a onehot
-# single (pick an example), combined (average), mode (majority vote), grouped (majority vote on group then average)
-seg_selection='grouped'
-# Will we deform the vollumes with piecewise linear displacement fields
-flag_deformation = True
-# Maximimum piecewise linear displacement in mm (excluding rotation + scaling)
-deformation_max = 5.0
 # Mode of the generator. Must be fa_v1 (linear interpolation of fa, nearest of v1) or rgb (linear on rgb)
 generator_mode = 'rgb'
-# generator_mode = 'fa_v1'
 # Resolution of diffusion data (only needed if randomizing resolution; we use it to compute width of blurring kernels)
 diffusion_resolution = 1.25
+
+
+## Network specific parameters
+# Batch size being volumes, it will probably be always 1...
+batchsize = 1
+# Size to which inputs will be cropped (use None to use whole volume)
+crop_size = 128
 # Number of levels in Unet (5 is good)
 n_levels = 5
 # Number of convolution + nonlinearity blocks per level (2 is good)
@@ -79,15 +87,14 @@ lr = 1e-4
 lr_decay = 0
 # Number of "pretraining" epochs where we use the L2 norm on the activations rather than Dice in the softmax (5-10)
 wl2_epochs = 5
-# Number of epocts with Dice
+# Number of epocs with Dice
 dice_epochs = 200
 # Steps per epoch (1000 is good)
 steps_per_epoch = 1000
 # Checkpoint file from which training will start (use None to start from scratch)
-#checkpoint = None
-checkpoint = '/home/henry/Documents/Brain/synthDTI/4henry/joint_diffusion_structural_seg/models/diffusion_thalamus_test_random_resolution3/wl2_001.h5'
-
-# torch.set_num_threads(8)
+checkpoint = None
+# frequency of saving model checkpoints (Dice iterations only)
+checkpoint_frequency = 5
 
 
 
